d870dcded248dbaab9b1983a2234a70ee6289cb0
SyruSTR <syrus_tr@mail.ru>
2020-10-15 13:50:49 +0800

    Add Tips

---
+++ Assets/Scripts/ChangeTips.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTips : MonoBehaviour
+{
+    [SerializeField] GameObject tipPrefab;
+    [SerializeField] private SwipeMenu swipeMenu;
+    private Image[] tipsImages;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (swipeMenu != null)
+        {
+            tipsImages = new Image[swipeMenu.ChildCount];
+            for (int i = 0; i < tipsImages.Length; i++)
+            {
+                tipsImages[i] = Instantiate(tipPrefab, transform).GetComponent<Image>();
+            }
+        }
+    }
+
+    public void ChangeStatus(int activePanelNumber)
+    {
+        for (int i = 0; i < tipsImages.Length; i++)
+        {
+            if (activePanelNumber == i)
+            {
+                tipsImages[i].color = Color.white;
+            }
+            else
+                tipsImages[i].color = Color.gray;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/SwipeMenu.cs
+++ Assets/Scripts/SwipeMenu.cs
@@ -1,15 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class SwipeMenu : MonoBehaviour
 {
-    [SerializeField] Scrollbar scrollbar;
-    float scroll_pos = 0f;
-    float[] pos;
-    int current_panel = 0;
-    float distance = 0f;
+    [SerializeField] private Scrollbar scrollbar;
+    private float scroll_pos = 0f;
+    private float[] pos;
+    private float distance = 0f;
+
+    public ChangePanel changePanel;
+
+    //public int CurrentPanel { get; private set; }
+    private int currentPanel = 0;
+    public int ChildCount { get { return transform.childCount; } }
     private void Awake()
     {
         scroll_pos = 0.5f;
@@ -25,7 +32,9 @@ public class SwipeMenu : MonoBehaviour
         {
             pos[i] = distance * i;
         }
-        
+
+        changePanel.Invoke(0);
+
     }
 
     // Update is called once per frame
@@ -33,27 +42,40 @@ public class SwipeMenu : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            scroll_pos = scrollbar.value;
+            scroll_pos = scrollbar.value * ChildCount;
+
+            if (scroll_pos > transform.childCount)
+            {
+                currentPanel = transform.childCount - 1;
+            }
+            else if (scroll_pos < 0)
+                currentPanel = 0;
+            else
+                currentPanel = Mathf.FloorToInt(scroll_pos);
+            changePanel.Invoke(currentPanel);
         }
         //Lerp Move panels
         else
         {
-            for (int i = 0; i < pos.Length; i++)
-            {
-                if(scroll_pos < pos[i] + (distance /2) && scroll_pos > pos[i] - (distance / 2))
-                {
-                    scrollbar.value = Mathf.Lerp(scrollbar.value, pos[i], 0.1f);
-                }
-            }
+            scrollbar.value = Mathf.Lerp(scrollbar.value, pos[currentPanel], 0.1f);
         }
-
-        //Lerp Change scale
-        //for (int i = 0; i < pos.Length; i++)
-        //{
-        //    if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
-        //    {
-        //        transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
-        //    }
-        //}
     }
+    public void NextPanel()
+    {
+        if (currentPanel < transform.childCount - 1)
+        {
+            currentPanel++;
+            changePanel.Invoke(currentPanel);
+        }
+    }
+    public void LastPanel()
+    {
+        if (currentPanel > 0)
+        {
+            currentPanel--;
+            changePanel.Invoke(currentPanel);
+        }
+    }
+    [System.Serializable]
+    public class ChangePanel : UnityEvent<int> { }
 }
